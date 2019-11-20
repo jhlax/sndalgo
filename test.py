ae5720432e62268842893c0f3d96eacbed0fe24d
jhlax <jhlax95@outlook.com>
2019-11-20 23:21:26 +0000

    added a test file to run to see a few examples in action.

---
+++ test.py
@@ -0,0 +1,13 @@
+from sndalgo.xenakis import Sieve
+
+major_scale = Sieve('-3@2&4|-3@1&4@1|3@2&4@2|-3@0&4@3')
+notes = major_scale.set(range(0,25))
+
+ots = Sieve('1@7') & '3@1' & (7, 3, False)
+
+print('major scale')
+print(major_scale, major_scale.set(range(0, 25)), sep='\n')
+print()
+print('digits of 137')
+print(ots, ots.simple, ots.set(range(0, 25)), sep='\n')
+print()
