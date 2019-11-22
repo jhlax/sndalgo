f493c2d7d13071943c22f41d3fc7417db49dfb13
jhlax <jhlax95@outlook.com>
2019-11-22 23:11:28 +0000

    added the pnote script!

--- sndalgo/pitch.py
+++ sndalgo/pitch.py
@@ -18,6 +18,7 @@ import numpy as np
 import numba as nb
 
 TUNING_FREQUENCY = 440.
+KEYS = ['C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab', 'A', 'A#/Bb', 'B']
 
 @nb.njit
 def ntof(note: int) -> float:
@@ -36,3 +37,21 @@ def fton(freq: float) -> float:
 
     return 69 + 12 * np.log2(freq / TUNING_FREQUENCY)
 
+def nokc(note: float) -> tuple:
+    """
+    returns a tuple containing (octave, key, cents)
+    """
+
+    octave = int((note // 12) - 1)
+    key = int(note) % 12
+    cents = (note - int(note)) * 100
+
+    return (octave, key, cents)
+
+def note_str(note: float) -> str:
+    """
+    returns the human-readable string for a note
+    """
+
+    o, k, c = nokc(note)
+    return f"{KEYS[k]}{o:d}" + (f" {c:+.3f}" if c else "")
--- test.py
+++ test.py
@@ -1,13 +1,18 @@
-from sndalgo.xenakis import Sieve
+# from sndalgo.xenakis import Sieve
 
-major_scale = Sieve("-3@2&4|-3@1&4@1|3@2&4@2|-3@0&4@3")
-notes = major_scale.set(range(0, 25))
+# major_scale = Sieve("-3@2&4|-3@1&4@1|3@2&4@2|-3@0&4@3")
+# notes = major_scale.set(range(0, 25))
 
-ots = Sieve("1@7") & "3@1" & (7, 3, False)
+# ots = Sieve("1@7") & "3@1" & (7, 3, False)
 
-print("major scale")
-print(major_scale, major_scale.set(range(0, 25)), sep="\n")
-print()
-print("digits of 137")
-print(ots, ots.simple, ots.set(range(0, 25)), sep="\n")
-print()
+# print("major scale")
+# print(major_scale, major_scale.set(range(0, 25)), sep="\n")
+# print()
+# print("digits of 137")
+# print(ots, ots.simple, ots.set(range(0, 25)), sep="\n")
+# print()
+
+import sndalgo.pitch as sa
+
+print("62:", sa.note_str(62))
+
