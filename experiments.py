0b8e15da89dfc325330ecc3a5f45bce1a8ad6510
jhlax <jhlax95@outlook.com>
2019-12-04 07:44:26 +0000

    moving forward in my thinking

--- experiments.py
+++ experiments.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+from dataclasses import dataclass
+
 import numpy as np
 import scipy as sp
 import termplotlib as tpl
@@ -13,43 +15,70 @@ from sndalgo.pitch import *
 from sndalgo.xenakis import *
 from sndalgo.filters import bfilter
 
+STD_SRATE = 48000
+STD_FREQ = 2
+STD_AMPL = 1
+STD_BSIZE = 8192
 
-bsize = 8192
-block = np.arange(bsize)
 
-# wt = S.waves.square_analog(1, block, 16, bsize)  # square wavetable
+@dataclass
+class Oscillator(object):
+    wtab: np.ndarray
+    srate: float = STD_SRATE
+    freq: float = STD_FREQ
+    pos0: int = 0
+    bsize: int = STD_BSIZE
 
-# print_sig(wt)  # show the wavetable
+    def __init__(self):
+        self.lu = lookup(1, self.bsize)
 
-f = 2  # frequency
-s = 48000  # sampling rate
-t = np.arange(s)
+    def process(self, samples, freq=None):
+        return self.lu(self.freq, samples)
 
-wform = waveform("square")
-oscil = lookup(wform)
-out = oscil(1, t)
 
-mod = waveform("harms", 1.0, 0, 0.99)
-mod = lookup(mod)
-mod = mod(3, t)
-mod = fgain(mod, 0.666)
-# mod = rectify(mod)
+wtab = waveform("square")
+osc = Oscillator()
+osc.wtab = wtab
 
-out = ring(out, mod)
-out = normalize(out)
+print(print_sig(osc.process(np.arange(48000)), False))
 
-out = lookup(out)
-out = out(2, t)
 
-highaf = bfilter(out, 4, s, btype="high", order=1, force_phase=True)
-lowaf = bfilter(out, 4, s, btype="low", order=1, force_phase=True)
+# bsize = 8192
+# block = np.arange(bsize)
 
-# print(*out, sep=' ')
+# # wt = S.waves.square_analog(1, block, 16, bsize)  # square wavetable
 
-print_sig(out, title="original")
+# # print_sig(wt)  # show the wavetable
 
-print_sig(highaf, title="highpass")
-print_sig(lowaf, title="lowpass")
+# f = 2  # frequency
+# s = 48000  # sampling rate
+# t = np.arange(s)
+
+# wform = waveform("square")
+# oscil = lookup(wform)
+# out = oscil(1, t)
+
+# mod = waveform("harms", 1.0, 0, 0.99)
+# mod = lookup(mod)
+# mod = mod(3, t)
+# mod = fgain(mod, 0.666)
+# # mod = rectify(mod)
+
+# out = ring(out, mod)
+# out = normalize(out)
+
+# out = lookup(out)
+# out = out(2, t)
+
+# highaf = bfilter(out, 4, s, btype="high", order=1, force_phase=True)
+# lowaf = bfilter(out, 4, s, btype="low", order=1, force_phase=True)
+
+# # print(*out, sep=' ')
+
+# print_sig(out, title="original")
+
+# print_sig(highaf, title="highpass")
+# print_sig(lowaf, title="lowpass")
 
 # sig = S.waves.lookup_oscil(f, t, wt, s)
 # sig2 = S.waves.lookup_oscil(f ** 3, t, wt, s)
